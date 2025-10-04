a5a2b6a67d9c02d9d6ffbc89b5840da427e9e30d
Adeel123-z <adeel451zahmed@gmail.com>
2025-10-04 12:19:54 +0500

    initial commit

---
+++ js/datatypesconfusion.js
@@ -0,0 +1,12 @@
+let score="Adeel";
+console.log(typeof score);
+let valueInNumber = Number(score);//in this line we are converting string to number
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+//hum es sai ya understand kar sakta hai ka string ko hum number ma convert nahi kat sakta
+let score1=123;
+console.log(typeof score1);
+let valueInNumbers = Number(score1);
+console.log(typeof valueInNumbers);
+console.log(valueInNumbers);
+//hum es sai ya understand kar sakta hai ka number ko hum number ma convert kar sakta hain
