eb3f01a61d99d7601e0b117616203e0d502c41cf
Adeel123-z <adeel451zahmed@gmail.com>
2025-10-04 10:08:06 +0500

    initial commit

---
+++ test.js
@@ -0,0 +1,6 @@
+const accountSid = '1234'
+let accountName = "anything"
+var accountCity ="New york"
+console.log(accountSid, accountName, accountCity);
+accountCity = "California"
+console.log(accountCity);
