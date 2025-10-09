070244dc9c8d51ef7dcaea033894ebf26e4764e0
Adeel123-z <adeel451zahmed@gmail.com>
2025-10-09 16:33:32 +0500

    object part2

---
+++ Chapter02/objectpart2.js
@@ -0,0 +1,18 @@
+const object1 ={
+
+}
+object1.name="Adeel"
+object1.age=22
+object1.email="adeel451zahmed@gmail.com"
+console.log(object1);
+const name={
+    name:"Sherry",
+    age:22,
+    fullname:{
+       username:{
+        firstname:"Adeel",
+        lastname:"Ahmed"
+       }
+    }
+}
+console.log(name);
