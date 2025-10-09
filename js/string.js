ff973fbea5719a5e81651f4879c4d31c7e972bfc
Adeel123-z <adeel451zahmed@gmail.com>
2025-10-09 09:41:39 +0500

    Strings

---
+++ js/string.js
@@ -0,0 +1,16 @@
+let name="Adeel Ahmed \n"
+let age=20
+console.log(name + age)
+// + this means is to concaneta (concanetation)
+//for concanetation modern way is to use backticks ``
+console.log(`My name is ${name} and my age is ${age}`)
+console.log(`My name is ${name} and my age is ${age+2}`)
+
+//Some Method of the String are 
+//touppercase
+//tolowercase
+//length
+//trim 
+//replace is used to replace the string (for example )
+let newString=name.replace("Adeel Ahmed","Sherry")
+console.log(newString)
