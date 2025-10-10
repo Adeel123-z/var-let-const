7a6637d275efdd7ad45dec93fa545fee3eb60ddd
Adeel123-z <adeel451zahmed@gmail.com>
2025-10-10 11:33:49 +0500

    Document Object Model

---
+++ Dom/test.js
@@ -0,0 +1,7 @@
+// let a=document.querySelector('ul')
+// let turngreen=a.querySelector('li')
+// turngreen.style.backgroundColor='green'
+// turngreen.style.color='white'
+let a=document.querySelector('ul')
+a.style.backgroundColor='blue'
+a.style.color="purple"
