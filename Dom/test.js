ff96c5618d1f6900ad7c09a0d60764d89e8b9fa0
Adeel123-z <adeel451zahmed@gmail.com>
2025-10-15 13:13:05 +0500

    Event Handling

--- Dom/test.js
+++ Dom/test.js
@@ -1,7 +1,17 @@
-// let a=document.querySelector('ul')
-// let turngreen=a.querySelector('li')
-// turngreen.style.backgroundColor='green'
-// turngreen.style.color='white'
 let a=document.querySelector('ul')
-a.style.backgroundColor='blue'
+let turngreen=a.querySelector('li')
+turngreen.style.backgroundColor='green'
+turngreen.style.color='white'
+let a=document.querySelector('ul')
+a.style.backgroundColor='white'
 a.style.color="purple"
+let b=document.querySelector('body')
+title.style.backgroundColor="yellow"
+b.style.backgroundColor="lightblue"
+//querySelectorAll
+let a =document.querySelectorAll('list')
+a[2].style.color="red"// this is yhe main dfference between queryselector and queryselectorall
+a[2].style.backgroundColor="black"
+a[2].style.fontSize="30px"
+a[2].style.fontWeight="bold"
+list .style.backgroundColor="black"
